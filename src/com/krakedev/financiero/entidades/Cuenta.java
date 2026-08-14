a694cbf12eeb2fc720c7844e23e1cbfd3afbe280
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-08-13 21:00:12 -0500

    Agregar composicion Cuenta-Cliente

--- src/com/krakedev/financiero/entidades/Cuenta.java
+++ src/com/krakedev/financiero/entidades/Cuenta.java
@@ -4,40 +4,50 @@ public class Cuenta {
     private String id;
     private double saldoActual;
     private String tipo;
-    
+    private Cliente propietario;
+
     public Cuenta(String id) {
         this.id = id;
         this.saldoActual = 0;
         this.tipo = "A";
     }
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public double getSaldoActual() {
-		return saldoActual;
-	}
+    public double getSaldoActual() {
+        return saldoActual;
+    }
 
-	public void setSaldoActual(double saldoActual) {
-		this.saldoActual = saldoActual;
-	}
+    public void setSaldoActual(double saldoActual) {
+        this.saldoActual = saldoActual;
+    }
 
-	public String getTipo() {
-		return tipo;
-	}
+    public String getTipo() {
+        return tipo;
+    }
 
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 
-	public void imprimir() {
+    public Cliente getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(Cliente propietario) {
+        this.propietario = propietario;
+    }
+
+    public void imprimir() {
         System.out.println("ID: " + id);
         System.out.println("Saldo: " + saldoActual);
         System.out.println("Tipo: " + tipo);
+        System.out.println("Cédula cliente: " + propietario.getCedula());
     }
 }
